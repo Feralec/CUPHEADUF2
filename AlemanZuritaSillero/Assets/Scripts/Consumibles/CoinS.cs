32f821743c3cb68b136b51d81a9a8d1eec34cb7d
Feralec <57528607+Feralec@users.noreply.github.com>
2020-02-08 18:09:02 +0100

    Coins
    Conteo de monedas

---
+++ AlemanZuritaSillero/Assets/Scripts/Consumibles/CoinS.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CoinS : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IncreaseCoins.numbersOfCoins += 1;
+        Destroy(gameObject);
+    }
+}
