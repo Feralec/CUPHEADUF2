b6be4d93195cadc8cde9db96b98db0b3426b3115
Feralec <57528607+Feralec@users.noreply.github.com>
2020-02-10 12:34:03 +0100

    Coin sounds

--- AlemanZuritaSillero/Assets/Scripts/Consumibles/CoinS.cs
+++ AlemanZuritaSillero/Assets/Scripts/Consumibles/CoinS.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 public class CoinS : MonoBehaviour
 {
+    public GameObject coinSound;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Instantiate(coinSound);
         IncreaseCoins.numbersOfCoins += 1;
         Destroy(gameObject);
     }
