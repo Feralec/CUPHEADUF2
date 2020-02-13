03b5e144526ecff5213138d74d7d8cb1e45e5de5
imzurita <59094657+imzurita@users.noreply.github.com>
2020-02-13 09:55:20 +0100

    import paquete enemigo
    enemy

--- AlemanZuritaSillero/Assets/Scripts/Controllers/Characters/PlayerController.cs
+++ AlemanZuritaSillero/Assets/Scripts/Controllers/Characters/PlayerController.cs
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         currentState.FixedExecute();
     }
+
     public void ChangeState(CharacterStates newState)
     {
         if (newState != null)
---
+++ AlemanZuritaSillero/Assets/Scripts/CursedEmojiController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursedEmojiController : Enemy
+{
+    [Range(0f, 10f)] public float speed = 2f;
+    public float perceptionRadius = 3f;
+    public float damage=50f;
+
+    private Animator anim;
+    private bool isAttacking;
+    private Rigidbody2D rb;
+    private Transform playerTransform;
+    private float sqrRadius;
+    PlayerController myPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        myPlayer = FindObjectOfType<PlayerController>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        sqrRadius = perceptionRadius * perceptionRadius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toPlayer = (playerTransform.position - transform.position);
+        if (toPlayer.sqrMagnitude<=sqrRadius)
+        {
+            transform.LookAt(transform.position+Vector3.forward,-toPlayer);
+            isAttacking = true;
+        } else
+        {
+            isAttacking=false;
+        }
+        anim.SetBool("isAttacking", isAttacking);
+    }
+
+    void FixedUpdate()
+    {
+        if (isAttacking)
+        {
+            transform.Translate(Vector3.down*Time.deltaTime*speed);
+        } else { //hago esto para que no se vaya volando al dejar de moverse
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, perceptionRadius);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+            myPlayer.pm.health -= 1f;
+            Debug.Log("TOCO CON PLAYER");
+            Debug.Log(myPlayer.pm.health);
+            Destroy(gameObject);
+        }
+    }
+}
