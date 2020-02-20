559fc4a892862b7c72e7a6d1b3dfd6d028efd444
SonicFoxPart2 <46996654+SonicFoxPart2@users.noreply.github.com>
2020-02-20 21:27:35 +0100

    hermano martillo uuuuh no está acabado pero lo estará

---
+++ AlemanZuritaSillero/Assets/Scripts/Models/Enemies/hammerBroModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "hammerBroModel", menuName = "Original/Enemies/Hammer Bro Model")]
+public class hammerBroModel : EnemyModel
+{
+
+}
