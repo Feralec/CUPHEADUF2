9231772863c178d3d365b586a6c43bd1cf0c48ff
Feralec <57528607+Feralec@users.noreply.github.com>
2020-02-22 16:34:08 +0100

    Win menu

---
+++ AlemanZuritaSillero/Assets/Scripts/Menus & UI/Menus/WinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+    public bool isPlaying = true;
+
+    public void PlayAgain()
+    {
+        //GameManager.GInstance.RestoreValues();
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
